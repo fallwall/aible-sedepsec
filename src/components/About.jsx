5b39d8b04a5cbb0bc171016fe4160b0ef82800e6
XX <fallwall19@gmail.com>
2020-01-23 17:19:53 -0500

    left right column adjustment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ export default function About() {
     <div className="about-container">
       <div className="about-left"></div>
       <div className="about-right">
-        <div className="section-title">About</div>
+        <div className="section-title section-title-white">Elbia Cespedes</div>
         <div className="about-content">
-          <p>Elbia is a New York City based writer.</p>
+          <p>She is a New York City based writer.</p>
         </div>
       </div>
     </div>
