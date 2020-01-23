fd3ecd16c2405178c0cca3f2455a6edbc43f3f86
XX <fallwall19@gmail.com>
2020-01-23 15:13:45 -0500

    menu to state control

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.css';
 import Entries from '../data/poems.json';
 
-export default function Menu() {
-  return (
-    <div className="menu-container">
-      <div className="menu-left"></div>
-      <div className="menu-right">{Entries.poems.map(e => <div key={e.id}>{e.title}</div>)}</div>
-    </div>
-  )
+export default class Menu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      entries: null
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      entries: Entries.poems
+    });
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="menu-container">
+        <div className="menu-left"></div>
+        <div className="menu-right">
+          {this.state.entries && this.state.entries.map(e => <div key={e.id}>{e.title}</div>)}
+        </div>
+      </div>
+    )
+  }
 }
