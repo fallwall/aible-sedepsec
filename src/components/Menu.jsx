6eefb6ff7ea3f870030aeec3db38de2718a689ad
XX <fallwall19@gmail.com>
2020-01-23 15:30:24 -0500

    menu items collapsable

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,7 +6,8 @@ export default class Menu extends Component {
   constructor() {
     super();
     this.state = {
-      entries: null
+      entries: null,
+      slected: null
     }
   }
 
@@ -16,15 +17,30 @@ export default class Menu extends Component {
     });
   }
 
-
-
+  selectItem = (id) => {
+    this.setState ({
+      selected: id
+    })
+  }
 
   render() {
     return (
       <div className="menu-container">
         <div className="menu-left"></div>
         <div className="menu-right">
-          {this.state.entries && this.state.entries.map(e => <div key={e.id}>{e.title}</div>)}
+          {this.state.entries && this.state.entries.map(e =>
+            <div key={e.id} className="menu-item">
+              <div className="menu-item-title"
+                onClick={()=>this.selectItem(e.id)}>
+                {e.title}
+              </div>
+              {
+                e.id === this.state.selected &&
+                <div className="menu-item-content">
+                  {e.content}
+                </div>
+              }
+            </div>)}
         </div>
       </div>
     )
