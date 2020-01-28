b3157d874e7180888575eddac97fbae3555db6b8
XX <fallwall19@gmail.com>
2020-01-27 22:28:34 -0500

    color

---
+++ build/precache-manifest.4684a990adf2715a917aed7e1226e733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cce940789bbfc5982e3bfb195da2ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bad7b2d8bab694a4da9",
+    "url": "/static/css/main.5691c305.chunk.css"
+  },
+  {
+    "revision": "923238f78f76b9e93939",
+    "url": "/static/js/2.e27d3630.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e27d3630.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bad7b2d8bab694a4da9",
+    "url": "/static/js/main.a33b4784.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  }
+]);
